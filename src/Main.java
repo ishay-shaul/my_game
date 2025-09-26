f8ef16eb998bd7c004812f7ba8721b632bc98022
ishay-shaul <ishay.shaul@mail.huji.ac.il>
2025-09-26 17:06:22 +0300

    added backround

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.components.CoordinateSpace;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.gui.rendering.AnimationRenderable;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 public class Main extends GameManager{
+
+    private final String backroundString = "C:/Users/ishay/JAVA/google_game/pictures/backround/41524.jpg";
 
     public Main() {
         super();
@@ -14,8 +22,16 @@ public class Main extends GameManager{
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Renderable gameBackroundImage = imageReader.readImage(backroundString, false);
+
+        Vector2 windowDimensions = windowController.getWindowDimensions();
+        GameObject backround = new GameObject(Vector2.ZERO, windowDimensions, gameBackroundImage);
+        backround.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        gameObjects().addGameObject(backround, Layer.BACKGROUND);
 
     }
+
+
 
     public static void main(String[] args){
         new Main().run();
